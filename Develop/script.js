c3a7e9c4d32d6afcf8c89b7d83589b7d2e3fd25b
Max Healy <maxhealy01@gmail.com>
2020-10-31 15:33:02 -0500

    Added task object, update color function

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,34 @@
 $("#currentDay").text(moment().format('dddd MMMM Do'));
+var tasks = {
+  "9": [],
+  "10": [],
+  "11": [],
+  "12": [],
+  "13": [],
+  "14": [],
+  "15": [],
+  "16": [],
+  "17": []
+};
 
+var textInput = "";
 // Set the color-changing functions for each task area
-for (i = 9; i < 18; i++){
-  if (moment().isBefore(23,"hours")){
-    $(".taskholder." + i).addClass("future")
+var updateColor = function(){
+  for (i = 9; i < 18; i++){
+    console.log("Chicken!")
+  if (moment().hour() < i){
+    $(".taskholder." + i).addClass("future");
   }
-  if (moment().isAfter(i,"hours")){
-    $(".taskholder." + i).addClass("past")
+  else if (moment().hour() > i){
+    $(".taskholder." + i).addClass("past");
   }
-  if (moment().hour() === i) {
-    $(".taskholder." + i).addClass("present")
+  else if (moment().hour() === i) {
+    $(".taskholder." + i).addClass("present");
   }
 }
+};
+
+
 // Create a function to make the taskholder areas turn into textareas on click.
 $(".taskholder").on("click", function(){
   // create a variable to shift classes from taskholder to textarea
@@ -24,22 +41,30 @@ $(".taskholder").on("click", function(){
   var textInput = $("<textarea>")
     .val(text)
     .addClass(classList);
-
+  textInput.value = document.querySelector("#taskholder")
   // replace taskholder div with textarea
   $(this).replaceWith(textInput);
   textInput.trigger("focus");
-  console.log(textInput.value)
+  console.log(textInput, textInput.value);
+ // $(".saveBtn").on("click", saveText(textInput.value))
 })
 
+var saveText = function(input){
+  console.log(input);
+}
 $(".saveBtn").on("click", function(textInput){
   // check if there's an <input> in dom 
   var text = $(this)
     .closest($(".row"))
     .find($("<textarea>"))
 
-  console.log(text);
+  console.log(textInput.value);
   // take text from textarea click function
   // save it to local storage
   // close input
   // take text, put into new p
 })
+
+// Call the functions to update colors on page load and every 5 minutes
+updateColor()
+setInterval(updateColor,300000);
